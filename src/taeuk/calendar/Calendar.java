9d8d67f062fdaacdff89cf5647e4995fc81bfcae
(tae uk) <(ss40967325.gmail.com)>
2021-04-19 18:53:04 +0900

    Implement find max days of the month

--- src/taeuk/calendar/Calendar.java
+++ src/taeuk/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package taeuk.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args) {
         System.out.println("일 월 화 수 목 금 토");
@@ -8,5 +10,14 @@ public class Calendar {
         System.out.println(" 8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("달을 입력하세요.");
+        int month = scanner.nextInt();
+
+
+        int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.printf("%d은 %d일 까지 있습니다.\n", month, maxDays[month - 1]);
+        scanner.close();
     }
 }
