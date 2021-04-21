84151a7c953509cdaa5c98485ed85db31503ec6a
(tae uk) <(ss40967325.gmail.com)>
2021-04-21 19:50:51 +0900

    add weekDay function

--- src/taeuk/calendar/Calendar.java
+++ src/taeuk/calendar/Calendar.java
@@ -17,28 +17,39 @@ public class Calendar {
         }else {
             return MAX_DAYS[month - 1];
         }
-
     }
 
-    public void printCalender(int year, int month) {
+    public void printCalender(int year, int month, int weekday) {
         System.out.printf("   <<%3d %2d>>\n", year,month);
         System.out.println("  SU MO TU WE TH FI SA");
         System.out.println("  --------------------");
 
-
         int maxDay = getmaxDaysOfMonth(year, month);
+        int count = 7 - weekday;
+        int delim = (count < 7) ? count : 0;
 
-        for (int i = 1; i<=maxDay; i++) {
+        //print blank space
+        for(int i = 0;  i <weekday; i++) {
+            System.out.print("   ");
+        }
+
+        //print first line
+        for(int i = 1; i <= count; i++) {
             System.out.printf("%3d", i);
-            if(i % 7 == 0) {
+        }
+        System.out.println();
+
+        //print second line to last
+
+        count++;
+        for (int i = count; i<=maxDay; i++) {
+            System.out.printf("%3d", i);
+            if(i % 7 == delim ) {
                 System.out.println();
             }
         }
         System.out.println();
-//        System.out.println(" 1  2  3  4  5  6  7");
-//        System.out.println(" 8  9 10 11 12 13 14");
-//        System.out.println("15 16 17 18 19 20 21");
-//        System.out.println("22 23 24 25 26 27 28");
+        System.out.println();
     }
 }
 
--- src/taeuk/calendar/Prompt.java
+++ src/taeuk/calendar/Prompt.java
@@ -1,37 +1,53 @@
 package taeuk.calendar;
-
 import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import sun.reflect.generics.tree.VoidDescriptor;
 
 import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT="cal>";
+//    @return 0 ~ 6 (0 = sun ~ 6 = sa)
 
+    public int parseDay(String week) {
+        if (week.equals("SU")) return 0;
+        else if (week.equals("MO")) return 1;
+        else if (week.equals("TU")) return 2;
+        else if (week.equals("WE")) return 3;
+        else if (week.equals("TH")) return 4;
+        else if (week.equals("FR")) return 5;
+        else if (week.equals("SA")) return 6;
+        else
+            return 0;
+    }
 
     public void runPrompt() {
         //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
 
-        int month = -1;
-        int year = -1;
-
+        int month = 1;
+        int year = 2021;
+        int weekday = 0;
         while (true) {
             System.out.println("년도를 입력하세요.");
             System.out.println("YEAR> ");
             year = scanner.nextInt();
+            if (year == -1) {
+                break;
+            }
             System.out.println("월을 입력하세요.");
             System.out.println("MONTH> ");
             month = scanner.nextInt();
-            System.out.println(PROMPT);
-            if(month == -1) {
-                break;
-            }
-            if(month > 12) {
+            System.out.println("첫번째 요일을 입력하세요. (SU, TU, MO, WE, TH, FR, SA)");
+            System.out.println("WEEKDAY> ");
+            String str_weekday = scanner.next();
+            weekday = parseDay(str_weekday);
+
+            if(month > 12 || month < 1) {
+                System.out.println("잘못된 입력입니다.");
                 continue;
             }
-            cal.printCalender(year, month);
+            cal.printCalender(year, month, weekday);
         }
         System.out.println("KKKK~");
         scanner.close();
