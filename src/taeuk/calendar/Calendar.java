ae3a6d5990a24103a57654721b0dcbc44895ab4e
(tae uk) <mee9203@naver.com>
2021-04-23 16:44:52 +0900

    add Auto Caelndar

--- src/taeuk/calendar/Calendar.java
+++ src/taeuk/calendar/Calendar.java
@@ -2,8 +2,8 @@ package taeuk.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    private static final int[] LEAP_MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] LEAP_MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public boolean isLeapYear(int year) {
         if(year % 4 == 0 && (year % 100 !=0 || year % 400 == 0))
@@ -13,16 +13,19 @@ public class Calendar {
     }
     public int getmaxDaysOfMonth(int year, int month) {
         if(isLeapYear(year)) {
-            return  LEAP_MAX_DAYS[month - 1];
+            return  LEAP_MAX_DAYS[month];
         }else {
-            return MAX_DAYS[month - 1];
+            return MAX_DAYS[month];
         }
     }
 
-    public void printCalender(int year, int month, int weekday) {
-        System.out.printf("   <<%3d %2d>>\n", year,month);
-        System.out.println("  SU MO TU WE TH FI SA");
+    public void printCalender(int year, int month) {
+        System.out.printf("%d년 %d월\n", year,month);
+        System.out.println("  일 월 화 수 목 금 토");
         System.out.println("  --------------------");
+
+        //get weekday automatically
+        int weekday = getWeekDay(year, month, 1);
 
         int maxDay = getmaxDaysOfMonth(year, month);
         int count = 7 - weekday;
@@ -51,7 +54,28 @@ public class Calendar {
         System.out.println();
         System.out.println();
     }
-}
 
+private int getWeekDay (int year, int month, int day) {
+    int syear = 1980;
+    final int STAMDARD_WEEKDAY = 1;
+
+    int count = 0;
+
+    for (int i =syear; i < year; i++) {
+        int delta = isLeapYear(i) ? 366 : 365;
+        count +=delta;
+    }
+
+//    System.out.println(count);
+    for (int i = 1; i < month; i++) {
+        int delta = getmaxDaysOfMonth(year, i);
+        count += delta;
+    }
+    count += day;
+
+    int weekday = (count + STAMDARD_WEEKDAY) % 7;
+    return weekday;
+  }
+}
 
 
--- src/taeuk/calendar/Prompt.java
+++ src/taeuk/calendar/Prompt.java
@@ -8,17 +8,6 @@ public class Prompt {
 
 //    @return 0 ~ 6 (0 = sun ~ 6 = sa)
 
-    public int parseDay(String week) {
-        if (week.equals("SU")) return 0;
-        else if (week.equals("MO")) return 1;
-        else if (week.equals("TU")) return 2;
-        else if (week.equals("WE")) return 3;
-        else if (week.equals("TH")) return 4;
-        else if (week.equals("FR")) return 5;
-        else if (week.equals("SA")) return 6;
-        else
-            return 0;
-    }
 
     public void runPrompt() {
         //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
@@ -27,27 +16,23 @@ public class Prompt {
 
         int month = 1;
         int year = 2021;
-        int weekday = 0;
+
         while (true) {
             System.out.println("년도를 입력하세요.");
-            System.out.println("YEAR> ");
+            System.out.println("> ");
             year = scanner.nextInt();
             if (year == -1) {
                 break;
             }
             System.out.println("월을 입력하세요.");
-            System.out.println("MONTH> ");
+            System.out.println("> ");
             month = scanner.nextInt();
-            System.out.println("첫번째 요일을 입력하세요. (SU, TU, MO, WE, TH, FR, SA)");
-            System.out.println("WEEKDAY> ");
-            String str_weekday = scanner.next();
-            weekday = parseDay(str_weekday);
 
             if(month > 12 || month < 1) {
                 System.out.println("잘못된 입력입니다.");
                 continue;
             }
-            cal.printCalender(year, month, weekday);
+            cal.printCalender(year, month);
         }
         System.out.println("KKKK~");
         scanner.close();
@@ -58,6 +43,6 @@ public class Prompt {
         //셀 실행
         Prompt p = new Prompt();
         p.runPrompt();
-        //test  commit
+
     }
 }
